aaf2d5f249e450c4813f80eb314b03258ddcebbd
Aleksandr Isakov <isakov.a.n@yandex.ru>
2022-04-12 19:44:08 +0300

    Добавил файлы проекта, добавил классы Praktikum и Account. В Account реализовал метод checkNameToEmboss.

---
+++ src/main/java/Account.java
@@ -0,0 +1,29 @@
+public class Account {
+
+    private final String name;
+
+    public Account(String name) {
+        this.name = name;
+    }
+
+    public boolean checkNameToEmboss() {
+        /*
+             Этот метод должен проверять, что сохранённая через конструктор строка соответствует требованиям.
+             Если строка удовлетворяет условиям, метод возвращает true, иначе — false.
+
+             Требования:
+                в строке не меньше 3 и не больше 19 символов,
+                в строке есть только один пробел,
+                пробел стоит не в начале и не в конце строки.
+         */
+
+        int spaces = (int) name.chars().filter(c -> c == ' ').count();
+        if (name.length() >= 3 && name.length() <= 19) {
+            if (spaces == 1 && !name.startsWith(" ") && !name.endsWith(" "))
+                return true;
+        }
+
+        return false;
+    }
+
+}
